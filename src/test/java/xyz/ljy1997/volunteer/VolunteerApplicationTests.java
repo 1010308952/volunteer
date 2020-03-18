737dfa9590b57062a776d653820df039d55ee1ff
linjianyi <Tecuhancup12_>
2020-03-18 17:21:50 +0800

    first commit

---
+++ src/test/java/xyz/ljy1997/volunteer/VolunteerApplicationTests.java
@@ -0,0 +1,13 @@
+package xyz.ljy1997.volunteer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VolunteerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
